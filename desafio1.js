a451265e01449018c1c6b10707bfbcc8b585401c
TsukiHyaku <105572109+TsukiHyaku@users.noreply.github.com>
2022-09-02 20:05:49 -0300

    Add files via upload

--- desafio1.js
+++ desafio1.js
@@ -1,13 +1,24 @@
-let  buzo= prompt("Bienvenido/a Soy Sara un algoritmo ayudante de la tienda TuAnimeShop.  \n Actualemete solo contamos con Buzos Personalizados en Stock. \n ¿Desea continuar? SI/NO")
+let tela
 
+const  costoBuzo = ( ) => {
+    tela =  prompt( 'Elija El tipo de Tela'+
+    '\n 1-Algodon' +
+    '  \n  2-Jogging'+
+    );
+}
+
+costoBuzo( );
+
+let  buzo= prompt("Bienvenido/a Soy Sara un algoritmo ayudante de la tienda TuAnimeShop.  \n Actualemete solo contamos con Buzos Personalizados en Stock. \n ¿Desea continuar? SI/NO")
 while( buzo.toLowerCase() == "si" ){
     let color=  prompt("Elija El Color que desea la Tela.")
-    let tela = prompt("Elija El tipo de Tela \n *Algodon *Jogging *Polar.")
+    let tela = parseInt (prompt("Elija El tipo de Tela\n 1-Algodon\n  2-Jogging\n  3-Polar."))
     let personaje = prompt("Elija el personaje que le gustaria en el estampado \n *Serena Tsukino *Kakashi Hatake *Monkey D. Luffy *Biscuit Krueger  *Edward Elric * Asuka Langley *Shoto Todoroki *Rei Ayanami")
     let pago = prompt("Ingrese el metodo de Pago \n *Debito *Credito *Mercado Pago *Otro")
-    
-    buzo = prompt(`EL RESULTADO FINAL DE SU PEDIDO ES EL SIGUIENTE \n Color del Buzo Será: ${color} \n Tipo de Tela Será: ${tela} \n Personaje: ${personaje} \n El Metodo de pago será con: ${pago} \n ¿Quiere confirmar su compra? SI/NO`)
+
+    buzo = prompt(`EL COSTO FINAL DE SU PEDIDO ES  EL SIGUIENTE \n Color del Buzo Será: ${color} \n Tipo de Tela Será: ${tela} \n Personaje: ${personaje} \n El Metodo de pago será con: ${pago} \n  Costo Final: ${costoFinal} ¿Quiere confirmar su compra? SI/NO`)
     if(buzo.toLowerCase() == "si"){
         alert("Muchas gracias por elegir TuAnimeShop Vuelva Pronto🤝.")
     }
 }
+
